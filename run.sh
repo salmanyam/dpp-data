f0ac23a7f863b0e863d6a45462a9f92f8cf8b8ee
Salman Ahmed <ahmedms@vt.edu>
2021-09-29 17:04:49 -0400

    added logs

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 PRG_NAME=$1
-LLVM_OPT=/home/danfeng/DPP/dpp-llvm/build/bin/opt
+LLVM_OPT=/home/salman/DPP/dpp-llvm/build/bin/opt
 
-$LLVM_OPT -S -passes="print-dpp-global" --dpp-rule="all" --dpp-output="/home/danfeng/DPP/dpp-data/logs/$PRG_NAME.txt" -disable-output < /home/danfeng/DPP/dpp-data/IRx86/$PRG_NAME.opt
+$LLVM_OPT -S -passes="print-dpp-global" --dpp-rule="all" --dpp-output="/home/salman/DPP/dpp-data/logs/$PRG_NAME.txt" -disable-output < /home/salman/DPP/dpp-data/IRx86/$PRG_NAME.opt
